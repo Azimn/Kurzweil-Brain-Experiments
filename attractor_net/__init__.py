524b81587f0c5da4e304922a09e7cb0739a6e089
Azimn <jratican@gmail.com>
2026-09-17 22:01:20 -0500

    Add attractor_net/__init__.py

---
+++ attractor_net/__init__.py
@@ -0,0 +1,4 @@
+from .encoding import ExperienceEncoder, ACTIONS, SCALAR_KEYS
+from .network import PlasticRecurrentAttractorNet
+from .world import DevelopmentalWorld, load_world
+from .evaluation import load_probes, run_probe_battery, phenotype_similarity
